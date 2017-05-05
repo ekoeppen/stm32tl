943cab07d938338ce4fcf69b396f71824a11be58
Eckhart Koeppen <eck@40hz.org>
2017-05-05 22:33:34 +0300

    Improve supported boards for CC110x example

--- examples/cc110x/receiver/main.cpp
+++ examples/cc110x/receiver/main.cpp
@@ -6,26 +6,25 @@ uint8_t packet[64];
 
 int main(void)
 {
-	HSE::init();
-	PLL::init();
-	SYSTICK::init();
+	BOARD::init();
 	SYSCLK::init();
-	POWER::init();
-	PORT_A::init();
-	PORT_B::init();
-	PORT_C::init();
-	LED_GREEN::set_high();
-	LED_RED::set_low();
-	EXT_INTERRUPT::init();
+	SYSTICK::init();
 	SPI::init();
+	CC1101_IRQ::init();
+	CC1101_CSN::init();
+	CC1101_CSN::set_high();
+	MISO_IN::init();
+	LED2::set_high();
+	EXT_INTERRUPT::init();
 	CC1101::reset();
+	MISO::init();
 	CC1101::init(cc110x_default_init_values, ARRAY_COUNT(cc110x_default_init_values));
 	CC1101::start_rx();
-	LED_GREEN::set_low();
+	LED2::set_low();
 	while (1) {
 		CC1101::template rx_buffer<TIMEOUT_NEVER>(packet, sizeof(packet));
-		LED_RED::set_high();
+		LED1::set_high();
 		SYSTICK::set_and_wait(50);
-		LED_RED::set_low();
+		LED1::set_low();
 	}
 }
--- examples/cc110x/sender/main.cpp
+++ examples/cc110x/sender/main.cpp
@@ -1,20 +1,38 @@
 #include "common.h"
 
+static const uint8_t data[7] = {0, 0, 0, 0, 0, 0, 0};
+
+static uint8_t buffer[64];
+
 int main(void)
 {
 	BOARD::init();
 	SYSCLK::init();
 	SYSTICK::init();
+	SPI::init();
 	CC1101_IRQ::init();
+	CC1101_CSN::init();
+	CC1101_CSN::set_high();
 	MISO_IN::init();
 	LED1::set_high();
 	EXT_INTERRUPT::init();
 	CC1101::reset();
+	MISO::init();
 	CC1101::init(cc110x_default_init_values, ARRAY_COUNT(cc110x_default_init_values));
-	LED2::set_high();
+	/*
+	CC1101_CSN::set_low();
+	SPI::transfer(CC1101_PATABLE);
+	SPI::transfer(0x03);
+	SPI::transfer(0xc0);
+	CC1101_CSN::set_high();
+	*/
+	CC1101::write_burst_reg(CC1101_PATABLE, (uint8_t *) "\x00\xc0\x00\x00\x00\x00\x00\x00", 8);
+	CC1101::write_reg(CC1101_FREND0, 0x11);
+	CC1101::read_regs(buffer);
+	LED1::set_low();
 	while (1) {
 		LED2::toggle();
-		CC1101::tx_buffer((uint8_t *) "\x55\xaa\x55\xaa", 4);
+		CC1101::tx_buffer(data, sizeof(data));
 		SYSTICK::set_and_wait(1000);
 	}
 }
